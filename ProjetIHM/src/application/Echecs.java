10c11563337fe337652e2ebda0830b988ccca466
JinChristine <jin.christine_0111@hotmail.com>
2024-11-23 16:40:04 +0100

    Update Echecs.java V1

--- ProjetIHM/src/application/Echecs.java
+++ ProjetIHM/src/application/Echecs.java
@@ -1,5 +1,40 @@
 package application;
 
-public class Echecs {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+
+public class Echecs {
+    private final Stage stage;
+
+    public Echecs(Stage stage) {
+        this.stage = stage;
+
+    }
+
+    public void afficher() {// Mettre le code d'affichage de Steven ici
+  
+        StackPane root = new StackPane();
+        Button button = new Button("Accueil");
+        
+        EventHandler <MouseEvent> allerAccueil = new EventHandler <MouseEvent> () {
+			@Override
+			public void handle (MouseEvent e) {
+				Accueil accueil= new Accueil(stage);
+				accueil.afficher(); // Naviguer vers Accueil
+			}
+		};
+		button.addEventHandler(MouseEvent.MOUSE_CLICKED, allerAccueil);
+		
+        Scene scene = new Scene(root, 400, 300);
+        root.getChildren().add(button);
+        stage.setScene(scene);
+        stage.setTitle("Calculatrice");
+        stage.show();
+    }
 }
