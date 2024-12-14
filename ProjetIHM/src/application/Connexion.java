5cfb957940951763bce314ea915a6c9fd665aef9
manal <147915371+Manalle@users.noreply.github.com>
2024-12-14 17:32:38 +0100

    Update Connexion.java

--- ProjetIHM/src/application/Connexion.java
+++ ProjetIHM/src/application/Connexion.java
@@ -31,74 +31,83 @@ public class Connexion extends Application {
         BorderPane conn = new BorderPane();
 
         HBox hbox = new HBox();
+        // ajuster le hbox en haut du BorderPane 
         conn.setTop(hbox);
 
-
+        // ajoute le logo en haut à gaute de l'interface 
         ImageView imLogo = new ImageView(new Image(getClass().getResourceAsStream("/Photo/Logo.png")));
         imLogo.setFitHeight(90);
         imLogo.setFitWidth(200);
 
-
+        // creation d'un espace entre les éléments 
         Region spacer = new Region();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
+        HBox.setHgrow(spacer, Priority.ALWAYS); // avec le setHgrow on assure que l'espace occupera toute la largeure disponible sur le hbox
 
+        // ajoute l'image de contact 
         ImageView imContact = new ImageView(new Image(getClass().getResourceAsStream("/Photo/contact.png")));
         imContact.setFitWidth(20);
         imContact.setFitHeight(20);
 
+        // creation d'un bouton contact 
         MenuButton contact = new MenuButton("Contact", imContact);
         contact.setPrefSize(130, 50);
+        // l'element de menu avec l'email du prof 
         MenuItem prof = new MenuItem("Contact par courriel : mr.crespin@profmath.fr");
         contact.getItems().add(prof);
 
 
         hbox.setPadding(new Insets(10, 20, 10, 20));
+        //on ajoute les elements au hbox
         hbox.getChildren().addAll(imLogo, spacer, contact);
 
 
         VBox root = new VBox(15);
         root.setPadding(new Insets(30));
-        root.setAlignment(Pos.CENTER);
+        root.setAlignment(Pos.CENTER); //on positione au centre 
         root.setStyle("-fx-background-color: #F5F5F5;");
 
-
+        // ajoute l'image de connexion 
         Image imageConn = new Image(getClass().getResourceAsStream("/Photo/LogoT.png"));
         ImageView image_connexion = new ImageView(imageConn);
         image_connexion.setFitWidth(240);
-        image_connexion.setPreserveRatio(true);
+        image_connexion.setPreserveRatio(true); // elle permet de maintenir le ratio de l'image lors de son redimensionnement 
 
-
+        // ajout du titre 
         Label titre = new Label("Se connecter");
         titre.setFont(Font.font("Calibri", 24));
-        titre.setTextAlignment(TextAlignment.CENTER);
+        titre.setTextAlignment(TextAlignment.CENTER); //on positione au centre
         titre.setStyle("-fx-font-weight: bold; -fx-text-fill: #2233AA;-fx-font-size: 20px;");
 
-
+        // ajout du sous-titre 
         Label sous_titre = new Label("Connectez-vous pour accéder à vos cours, ressources.");
         sous_titre.setFont(Font.font("Calibri", 14));
-        sous_titre.setTextAlignment(TextAlignment.CENTER);
+        sous_titre.setTextAlignment(TextAlignment.CENTER); //on positione au centre
 
-
+        // ajout d'un label email
         Label email = new Label("Email");
         email.setStyle("-fx-font-weight: bold; -fx-text-fill: #2233AA; -fx-font-size: 16px;");
+        // definit un champs pour saisir l'email
         TextField zonetext = new TextField();
         zonetext.setPromptText("exemple@exemple.com");
         zonetext.setMaxWidth(300);
         zonetext.setStyle("-fx-border-color: gray; -fx-border-radius: 3px; -fx-font-size: 19px;");
 
-
+        // ajout d'un label mot de passe
         Label mot_de_passe = new Label("Mot de passe");
         mot_de_passe.setStyle("-fx-font-weight: bold; -fx-text-fill: #2233AA; -fx-font-size: 16px;");
+        
+        // definit un champ pour saisir le mot de passe
         PasswordField mdp = new PasswordField();
         mdp.setStyle("-fx-border-color: gray; -fx-border-radius: 3px; -fx-font-size: 19px;");
         mdp.setMaxWidth(300);
 
+        // ajout d'un bouton pour se connecter
         Button se_connecter = new Button("Se connecter");
         se_connecter.setStyle("-fx-background-color: #d6d6ce; -fx-text-fill: #2233AA; -fx-font-size: 14px; -fx-font-weight: bold;");
         
         Label messageErr = new Label();
         messageErr.setStyle("-fx-text-fill: red;");
-
+        //Evenement pour le bouton se connecter
         se_connecter.setOnAction(new EventHandler<ActionEvent>() {
         	@Override
         	public void handle(ActionEvent event) {
@@ -123,11 +132,11 @@ public class Connexion extends Application {
         	}
         });
 
-        
+        // ajouts des itemps dans le root
         root.getChildren().addAll(image_connexion, titre, sous_titre, email, zonetext, mot_de_passe, mdp, se_connecter, messageErr);
-        
         conn.setCenter(root);
 
+        // configuration de scene
         Scene scene = new Scene(conn, 900, 800);
         scene.getStylesheets().add(getClass().getResource("styleCahierTexte.css").toExternalForm());
         primaryStage.setTitle("Connexion");
